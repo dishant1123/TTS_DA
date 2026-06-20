5a71126ff3225af4885de495182b5fd181099b96
dishant1123 <dishantshah1417@gmail.com>
2026-06-20 10:00:19 +0530

    ....

---
+++ pandas/20june.py
@@ -0,0 +1,10 @@
+"""
+create  dataframe  : 
+    id , name , age , salary
+    
+1. head () , tail ()
+2. find  missing value  in age and salary  and  print  count  of  missing value and then fill  with mean value
+3. convert name  in to upper case 
+4. if salary >50000 then  person  included in  A category  else  B category
+
+"""
