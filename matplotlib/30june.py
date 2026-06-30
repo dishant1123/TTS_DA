285389e905f497e31c7835fd5389dd556a8abf37
dishant1123 <dishantshah1417@gmail.com>
2026-06-30 11:21:35 +0530

    ....

--- matplotlib/30june.py
+++ matplotlib/30june.py
@@ -33,27 +33,53 @@ plt.grid(True)
 plt.show()
 """
 # barh charts :
-
+"""
 df = pd.DataFrame({
     "sectors" :["Government","Education","Health","Industry","Agriculture"],
-    "population":[100,200,150,400,350]
+    "population":[100,200,150,400,900]
 })
 bars =plt.barh(df["sectors"],df["population"],color="skyblue",height=0.5)
-width = 0.5
+width = 0.8
 plt.title("population")
 plt.xlabel("population")
 plt.ylabel("sectors")
 for i in bars:  # bars = [ram ,sita ,ravan ,laxman ,dhan]
     plt.text(
-        i.get_y() + i.get_width()/2,  # get_x() ===> coordinate x
-        i.get_height()+1,
-        i.get_height(),
-        ha="center",   # horizontal alignment
+        i.get_width(),   # x 
+        i.get_y() + i.get_height()/2,  # y
+        # i.get_height()+1,
+        # i.get_height(),
+        i.get_width(),  # text
+        ha="left",   # horizontal alignment
+        va="center",   # vertical alignment
         fontsize=10,
         color="black",
         style="italic"
     )
-
-
 plt.grid(True)
 plt.show()
+"""
+# plt.ylim(0,100)   # change the y axis range.  
+
+# pie charts : percent divide  
+
+"""
+mobile =["iphone","android","windows","samsung","htc"]
+shares = [78,56,34,61,20]
+
+plt.pie(shares,labels=mobile,autopct="%1.1f%%")
+plt.title("mobile share")
+plt.show()
+"""
+
+# donut charts :
+mobile =["iphone","android","windows","samsung","htc"]
+shares = [78,56,34,61,20]
+
+plt.pie(shares,labels=mobile,autopct="%1.1f%%",wedgeprops={'width':0.6})
+plt.title("mobile share")
+plt.show()
+
+
+# next : overlapping function  , wraptext function  
+# task  : 1 using  flipkart data set : bar chart ,  barh , pie , donut 
